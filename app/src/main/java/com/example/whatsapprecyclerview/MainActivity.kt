a12a89494a729b0874cf3b470e2678374dcb60a0
dipeshgupta <dipeshguptadeveloper@gmail.com>
2023-05-04 22:42:47 +0530

    add functionality added

--- app/src/main/java/com/example/whatsapprecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/whatsapprecyclerview/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.whatsapprecyclerview
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.whatsapprecyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    lateinit var chatAdapter: ChatRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,8 +59,48 @@ class MainActivity : AppCompatActivity() {
 
         binding.rcView.layoutManager = LinearLayoutManager(this@MainActivity)
 
-        val chatAdapter = ChatRecyclerAdapter(this, arrChats)
+        chatAdapter = ChatRecyclerAdapter(this, arrChats)
 
         binding.rcView.adapter = chatAdapter
+
+        binding.fabAdd.setOnClickListener {
+
+            val addDialog = Dialog(this@MainActivity)
+            addDialog.setContentView(R.layout.chat_update_dialog)
+
+            val edtChatName = addDialog.findViewById<TextView>(R.id.edtChatName)
+            val btnAdd = addDialog.findViewById<Button>(R.id.btnAdd)
+            val btnCancel = addDialog.findViewById<Button>(R.id.btnCancel)
+
+
+            btnAdd.setOnClickListener {
+                val chatName = edtChatName.text.toString()
+
+                if (chatName.isNotBlank()) {
+                    arrChats.add(
+                        ChatModel(
+                            R.drawable.ic_launcher_background,
+                            chatName,
+                            "Happy",
+                            "00:00 AM"
+                        )
+                    )
+
+                    chatAdapter.notifyItemInserted(arrChats.size - 1)
+                    binding.rcView.scrollToPosition(arrChats.size - 1)
+                }
+                addDialog.dismiss()
+
+            }
+
+            btnCancel.setOnClickListener {
+                addDialog.dismiss()
+            }
+
+
+            addDialog.show()
+
+        }
+
     }
 }
